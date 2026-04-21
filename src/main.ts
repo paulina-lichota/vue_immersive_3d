c17bdd210434c2b4a0527bddda344c222e3a374b
paulich07 <50242426+paulich07@users.noreply.github.com>
2026-04-21 17:22:42 +0200

    boilerplate

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
-import Tres from '@tresjs/core';
+import { templateCompilerOptions } from '@tresjs/core';
 import App from './App.vue'
 
 const app = createApp(App);
-app.use(Tres);
-app.mount('#app')
+app.mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { templateCompilerOptions } from '@tresjs/core';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-     vue(),
+     vue({
+      ...templateCompilerOptions
+     }),
   ],
 })
